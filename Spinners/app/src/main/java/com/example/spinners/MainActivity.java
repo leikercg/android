78b39c50db34fdbd31227c14f7eb0954d8c0fc0a
Leiker Castillo <leikercg@gmail.com>
2024-10-22 13:25:03 +0200

    Menus

--- Spinners/app/src/main/java/com/example/spinners/MainActivity.java
+++ Spinners/app/src/main/java/com/example/spinners/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.spinners;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     Spinner lista;
     TextView tv;
 
+    EditText num1, num2; // son los numero introducidos
+    Button boton; // Es el boton de calcular
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +36,12 @@ public class MainActivity extends AppCompatActivity {
         lista = findViewById(R.id.spinner);
         tv = findViewById(R.id.tv);
 
+        num1=findViewById(R.id.num1);
+        num2=findViewById(R.id.num2);
+        boton= findViewById(R.id.button);
 
-       // Adaptador en JAVA
+
+        // Adaptador en JAVA
         String[] datos = new String[]{"Elem1", "Elem2", "Elem3", "Elem4", "Elem5"};
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos);
         lista.setAdapter(adaptador);
@@ -43,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
         lista.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), datos[i], Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(), datos[i], Toast.LENGTH_SHORT).show();
+
                 tv.setText(datos[i]);
             }
 
@@ -53,6 +67,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.menu_principal, menu);
+
+        return true;
+
+        //getMenuInflater().inflate(R.menu.activity_main, menu);
+        //return true; esto es del PDF
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.opcion_sumar) {
+            tv.setText("SUMAR");
+        } else if (item.getItemId() == R.id.opcion_restar) {
+            tv.setText("RESTAR");
+        } else if (item.getItemId() == R.id.opcion_multiplicar) {
+            tv.setText("MULTIPLICAR");
+        } else if (item.getItemId() == R.id.opcion_dividir) {
+            tv.setText("DIVIDIR");
+        }
+        return true;
+    }
+
 }
