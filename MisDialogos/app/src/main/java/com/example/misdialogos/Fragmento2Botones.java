35f1012ed3f41cccf7bcff083c86f0974e4e000c
Leiker Castillo <leikercg@gmail.com>
2024-10-29 13:24:38 +0100

    Fragmentos

---
+++ MisDialogos/app/src/main/java/com/example/misdialogos/Fragmento2Botones.java
@@ -0,0 +1,28 @@
+package com.example.misdialogos;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class Fragmento2Botones extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder ventana= new AlertDialog.Builder(getActivity());
+        ventana.setTitle("DIÁLOGO CON FRAGMENTO");
+        ventana.setMessage("Diálogo que utiliza un fragmento");
+        // TODO: 29/10/2024   Poner ícono
+
+        ventana.setNegativeButton("SÍ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), "Mensaje desde fragmento", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return ventana.create();
+    }
+}
--- MisDialogos/app/src/main/java/com/example/misdialogos/MainActivity.java
+++ MisDialogos/app/src/main/java/com/example/misdialogos/MainActivity.java
@@ -1,19 +1,68 @@
 package com.example.misdialogos;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
+    AlertDialog.Builder ventana;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Inicializa el TextView aquí
+        tv = findViewById(R.id.tv);
+    }
+
+    public void clickFragmento(View v){
+        Fragmento2Botones f2b = new Fragmento2Botones();
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        f2b.show(getSupportFragmentManager(),"xxx");
+    }
+
+    public void clickDialogoMensaje(View v) {
+        ventana = new AlertDialog.Builder(this);
+        ventana.setTitle("DIALOGO SIMPLE");
+        ventana.setMessage("Mensaje de dialogo simple");
+
+        // Creando el primer diálogo
+        ventana.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tv.setText("Dialogo simple aceptado");
+            }
+        });
+        ventana.show(); // Muestra el diálogo simple
+
+        // Creando el segundo diálogo con dos botones
+        AlertDialog.Builder dialogoDosBotones = new AlertDialog.Builder(this);
+        dialogoDosBotones.setTitle("Dialogo dos botones");
+        dialogoDosBotones.setMessage("Este es un mensaje que usa 2 botones");
+
+        dialogoDosBotones.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tv.setText("Botón sí pulsado");
+            }
+        });
+        dialogoDosBotones.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tv.setText("Botón no pulsado");
+            }
+        });
+
+        dialogoDosBotones.show(); // Muestra el diálogo con dos botones
     }
 }
